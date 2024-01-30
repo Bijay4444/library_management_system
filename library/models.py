5bc391e06a661d5f7e3f33c014b9e758f77c9ad8
Bijay Khadka <bk006822@gmail.com>
2024-01-30 15:55:49 +0545

    Setting up task

---
+++ library/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+#User model for storing user details
+class User(models.Model):
+    UserID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    Email = models.EmailField(unique=True)
+    MembershipDate = models.DateField()
+
+    def __str__(self):
+        return self.Name
+
+#Book model for storing book details
+class Book(models.Model):
+    BookID = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=13, unique=True)
+    PublishedDate = models.DateField()
+    Genre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Title
+
+#BookDetails model for storing book details
+class BookDetails(models.Model):
+    DetailsID = models.AutoField(primary_key=True)
+    Book = models.OneToOneField(Book, on_delete=models.CASCADE)
+    NumberOfPages = models.IntegerField()
+    Publisher = models.CharField(max_length=255)
+    Language = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.Book.Title} Details"
+    
+#BorrowedBooks models for storing borrow details 
+class BorrowedBooks(models.Model):
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE)
+    BookID = models.ForeignKey(Book, on_delete=models.CASCADE)
+    BorrowDate = models.DateField()
+    ReturnDate = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.User.Name} borrowed {self.Book.Title}"
