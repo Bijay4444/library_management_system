b3cf5b072ff6810271599460b026a5e537524f24
Bijay Khadka <bk006822@gmail.com>
2024-01-30 16:08:50 +0545

    added api endpoints

--- library/views.py
+++ library/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Book, BookDetails, BorrowedBooks
+from .serializers import UserSerializer, BookSerializer, BookDetailsSerializer, BorrowedBooksSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+class BookDetailsViewSet(viewsets.ModelViewSet):
+    queryset = BookDetails.objects.all()
+    serializer_class = BookDetailsSerializer
+    
+class BorrowedBooksViewSet(viewsets.ModelViewSet):
+    queryset = BorrowedBooks.objects.all()
+    serializer_class = BorrowedBooksSerializer
